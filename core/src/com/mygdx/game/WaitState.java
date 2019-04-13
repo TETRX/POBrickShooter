0241c8e37887b7dcd66acff2b3689cc9dea9ffc2
Weronika Lorenczyk <weronikalorenczyk@wp.pl>
2019-04-13 09:39:46 +0200

    More bullets, all blocks, classes Bullet,Block,EndgameState

--- core/src/com/mygdx/game/WaitState.java
+++ core/src/com/mygdx/game/WaitState.java
@@ -5,39 +5,73 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static java.lang.Thread.sleep;
 
 public class WaitState extends State {
     protected WaitState(StateHandler sh) {
         super(sh);
         this.sh=sh;
+        createBlocks();
     }
     ShapeRenderer shapeRenderer=new ShapeRenderer();
     StateHandler sh;
-    Vector2 start=new Vector2(400,0);
+    Vector2 start=new Vector2(400,10);
     Vector2 destination=new Vector2();
+    Vector2 velocity;
+    List<Bullet> listOfBullets=new ArrayList<Bullet>();
+    Block [][] arrOfBlocks=new Block[5][5];
+    int round=0;
 
-    int blocks[]={5,5,5,5,5};
+
+    int allBlocksX=50;
+    int allBlocksY=200;
+    int allBloocksHeight=300;
+    int allBlocksWidth=700;
+
+    void createBlocks(){
+
+        int width=allBlocksWidth/5;
+        int height=allBloocksHeight/5;
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                arrOfBlocks[i][j]=new Block(allBlocksX+i*width+2,allBlocksY+j*height+2,width-4,height-4,1);
+            }
+        }
+
+    }
+
 
     @Override
     public void update(float gameLoopTime) {
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.setColor(1,1,1,1);
-        shapeRenderer.circle(400,10,15F);
+        start.set(400,10);
         for(int i=0;i<5;i++){
-            if(blocks[i]>0){
-                shapeRenderer.rect(120*i,400,100,50);
+            for(int j=0;j<5;j++){
+                arrOfBlocks[i][j].render();
             }
         }
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(1,1,1,0);
+        shapeRenderer.circle(400,5,15);
         shapeRenderer.line(400,0,Gdx.input.getX(),600-Gdx.input.getY());
         shapeRenderer.end();
 
             if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
                     destination.x = Gdx.input.getX();
                     destination.y = 600-Gdx.input.getY();
-
+                    velocity=destination.sub(start);
                     System.out.println("dest set"+start+"  "+destination);
-                    sh.add(new PlayState(sh,start,destination,this));
+                    round ++;
+                    listOfBullets.add(new Bullet(this));
+                    float i=0;
+                    for(Bullet x : listOfBullets){
+                        x.set(start.mulAdd(velocity,i),velocity);
+                        i-=0.1;
+                    }
+
+                    sh.add(new PlayState(sh,this));
 
             }
     }
